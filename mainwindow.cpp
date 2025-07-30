4bca6e7477f9160310b56cdb08de8a60e5e20d65
yuxuan <xuanyuong@gmail.com>
2025-07-30 12:20:44 +0800

    Set up db schema + query testing from app succeeded + db file dir management

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include <QSqlDatabase>
 #include <QSqlError>
 #include <QMessageBox>
+#include <QSqlQuery>
+#include <QSqlDriver>
+#include <QDate>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,6 +15,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");  // Use as default connection.
+    // create yx_routinetracker.db and create schema if it never existed.
     db.setDatabaseName("yx_routinetracker.db");
     bool ok = db.open();
     if (!ok) {
@@ -37,5 +41,45 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::test() {
-    QSqlDatabase db = QSqlDatabase::database();
+    {
+        QSqlQuery query;  // Queries default database when unspecified.
+        query.setForwardOnly(true);  // Used during cases where only next() and seek() (forward browsing) is utilized, to improve query speed.
+        bool ok = query.exec(
+            "SELECT id, name "
+            "FROM routine "
+            "WHERE is_active = 1;"
+            );
+        if (!ok) {
+            QMessageBox::critical(this, "Db Error", query.lastError().text());
+            return;
+        }
+        while (query.next()) {
+            qint64 id = query.value(0).toLongLong();
+            QString name = query.value(1).toString();
+            qDebug() << "id: " << id << "  name: " << name;
+        }
+    }
+    {
+        QString startDate = QDate::currentDate().addDays(-7).toString(Qt::ISODate);
+        QString endDate = QDate::currentDate().toString(Qt::ISODate);
+        QSqlQuery query;
+        query.setForwardOnly(true);
+        query.prepare(
+            "SELECT date, routine_id "
+            "FROM records "
+            "WHERE date BETWEEN :start_date AND :end_date "
+            "ORDER BY date ASC");
+        query.bindValue(":start_date", startDate);
+        query.bindValue(":end_date", endDate);
+        bool ok = query.exec();
+        if (!ok) {
+            QMessageBox::critical(this, "Db Error", query.lastError().text());
+            return;
+        }
+        while (query.next()) {
+            QDate date = QDate::fromString(query.value(0).toString(), Qt::ISODate);
+            int routine_id = query.value(1).toLongLong();
+            qDebug() << "date: " << date << "  routine_id: " << routine_id;
+        }
+    }
 }
